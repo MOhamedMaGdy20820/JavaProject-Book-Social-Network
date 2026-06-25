fdaa8531e32cfa8346811161ee6e992756a76bf5
Mohamed Magdy <120112354+MOhamedMaGdy20820@users.noreply.github.com>
2026-06-25 14:41:03 +0300

    Refactor authentication and user management: integrate Keycloak, update user-related entities, and adjust security configurations

--- book-network-ui/src/app/services/keycloak/keycloak.service.ts
+++ book-network-ui/src/app/services/keycloak/keycloak.service.ts
@@ -21,7 +21,7 @@ export class KeycloakService {
     if (!this._keycloak) {
       this._keycloak = new Keycloak({
         url: 'http://localhost:9090',
-        realm: 'book-social-network',
+        realm: 'book-social-network.json',
         clientId: 'bsn'
       });
     }
